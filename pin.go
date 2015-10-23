91e3d7ff4e71430e90ec9ff3099d09a8258d5602
Wally <wally@imwally.net>
2015-10-23 16:58:52 -0400

    rework command and argument handling

--- pin.go
+++ pin.go
@@ -22,78 +22,81 @@ var (
 	token string
 )
 
+// Default number of bookmarks to display
+const COUNT int = 50
+
 // Add checks flag values and encodes the GET URL for adding a bookmark.
 func Add(p pinboard.Post) {
 
-    p.Description = *titleFlag
+	p.Description = *titleFlag
 
-    if *privFlag {
-        p.Shared = "no"
-    }
+	if *privFlag {
+		p.Shared = "no"
+	}
 
-    if *readFlag {
-        p.Toread = "yes"
-    }
+	if *readFlag {
+		p.Toread = "yes"
+	}
 
-    if *extFlag != "" {
-        p.Extended = *extFlag
-    }
+	if *extFlag != "" {
+		p.Extended = *extFlag
+	}
 
-    if *tagFlag != "" {
-        p.Tags = *tagFlag
-    }
+	if *tagFlag != "" {
+		p.Tags = *tagFlag
+	}
 
-    p.Encode()
-    err := p.Add()
-    if err != nil {
-        fmt.Println(err)
-    }
+	p.Encode()
+	err := p.Add()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-// Delete will delete the URL value of the -d flag.
+// Delete will delete the URL specified.
 func Delete(p pinboard.Post) {
-    p.Encode()
-    err := p.Delete()
-    if err != nil {
-        fmt.Println(err)
-    }
+	p.Encode()
+	err := p.Delete()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-// Show will list the most recent bookmarks. The -show flag indicates how many
-// bookmarks to show with a max of up to 100 bookmarks.
+// Show will list the most recent bookmarks.
 func Show(p pinboard.Post) {
-    if *tagFlag != "" {
-        p.Tag = *tagFlag
-    }
+	if *tagFlag != "" {
+		p.Tag = *tagFlag
+	}
 
-    p.Encode()
+	p.Count = COUNT
+	p.Encode()
 
-    recent := p.ShowRecent()
+	recent := p.ShowRecent()
 
-    if *longFlag {
-        for _, v := range recent.Posts {
-            var shared string
-            if v.Shared == "no" {
-                shared = "*"
-            }
-            fmt.Println(shared + v.Description)
-            fmt.Println(v.Href)
-            fmt.Println(v.Tags, "\n")
-        }
-    } else {
-        for _, v := range recent.Posts {
-            fmt.Println(v.Href)
-        }
-    }
+	if *longFlag {
+		for _, v := range recent.Posts {
+			var shared string
+			if v.Shared == "no" {
+				shared = "*"
+			}
+			fmt.Println(shared + v.Description)
+			fmt.Println(v.Href)
+			fmt.Println(v.Tags, "\n")
+		}
+	} else {
+		for _, v := range recent.Posts {
+			fmt.Println(v.Href)
+		}
+	}
 }
 
 // TokenIsSet will check to make sure an authentication token is set before
 // making an API calls.
 func TokenIsSet() bool {
-    if token == "" {
-        return false
-    }
-    return true
+	if token == "" {
+		return false
+	}
+	return true
 }
 
 func init() {
@@ -104,44 +107,73 @@ func init() {
 
 	content, err := ioutil.ReadFile(u.HomeDir + "/.pinboard")
 	if err != nil {
-        fmt.Println("No authorization token found. Please add your authorization token to ~/.pinboard")
-	} 
+		fmt.Println("No authorization token found. Please add your authorization token to ~/.pinboard")
+	}
 
 	token = string(content)
 }
 
-func main() {
-	
-	if !TokenIsSet() {
-		return
-	}
-	
-	var p pinboard.Post
-	p.Token = token
-	
-	flag.Parse()
+func runCmd(cmds []string) {
 
+	flag.Parse()
 	if flag.NArg() < 1 {
 		fmt.Fprintf(os.Stderr, "No command is given.\n")
+		return
 	}
-	
-	cmd := flag.Arg(0)
-	
-	if cmd == "add" {
+
+	cmdName := flag.Arg(0)
+
+	var found bool
+	for _, cmd := range cmds {
+		if cmdName == cmd {
+			found = true
+		}
+	}
+
+	if !found {
+		fmt.Fprintf(os.Stderr, "Command %s not found.\n", cmdName)
+		return
+	}
+
+	// Initialise a post
+	var p pinboard.Post
+	p.Token = token
+
+	if cmdName == "add" {
+		if flag.NArg() < 2 {
+			fmt.Fprintf(os.Stderr, "Not enough arguments.\n")
+			return
+		}
 		args := flag.Args()[2:]
 		p.URL = flag.Args()[1]
 		options.Parse(args)
 		Add(p)
 	}
 
-	if cmd == "ls" {
+	if cmdName == "ls" {
 		args := flag.Args()[1:]
 		options.Parse(args)
 		Show(p)
 	}
 
-	if cmd == "rm" {
+	if cmdName == "rm" {
+		if flag.NArg() < 2 {
+			fmt.Fprintf(os.Stderr, "Not enough arguments.\n")
+			return
+		}
 		p.URL = flag.Args()[1]
 		Delete(p)
 	}
+
+}
+
+func main() {
+
+	if !TokenIsSet() {
+		return
+	}
+
+	cmds := []string{"add", "rm", "ls"}
+
+	runCmd(cmds)
 }
