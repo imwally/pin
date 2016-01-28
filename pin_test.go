35c5d4de206a6b77a2d1724cf82e817e64aa290f
Wally <wally@imwally.net>
2016-01-28 12:53:02 -0500

    Add tests.

---
+++ pin_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func spinner() {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(1 * time.Millisecond)
+		}
+	}
+}
+
+func TestGetPageTitle(t *testing.T) {
+	urls := []string{
+		"https://divan.github.io/posts/go_concurrency_visualize/",
+		"https://github.com/imwally/pin/blob/master/pin.go",
+		"https://amazon.com",
+		//"https://laskdjflajlhgalkghalgkl.com",
+	}
+
+	for _, url := range urls {
+		go spinner()
+		fmt.Printf("Trying %s: \n", url)
+		title, err := PageTitle(url)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		fmt.Printf("\r[\u2713] %s\n", title)
+	}
+}
